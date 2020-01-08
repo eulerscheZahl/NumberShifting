6781bad5986be284bf26aebdc14017711948bbc1
eulerscheZahl <eulerschezahl@yandex.com>
2020-01-08 18:11:28 +0100

    put comments back in

--- solver/submit.py
+++ solver/submit.py
@@ -9,6 +9,7 @@ import io
 import requests
 import subprocess
 
+# login to CodinGame and get submit ID
 session = requests.Session()
 r = session.post('https://www.codingame.com/services/Codingamer/loginSiteV2', json=[email, password, True])
 userId = r.json()['codinGamer']['userId']
@@ -16,7 +17,9 @@ userId = r.json()['codinGamer']['userId']
 r = session.post('https://www.codingame.com/services/Puzzle/generateSessionFromPuzzlePrettyId', json=[userId, "number-shifting", False])
 handle = r.json()['handle']
 
+# for each level of the game
 while True:
+	# run the solver on level.txt and save output to solution.txt
 	subprocess.run(program_execute + " < level.txt > solution.txt", shell=True)
 	with open('level_password.txt', 'r') as f:
 		level_pass = f.read().strip()
@@ -27,6 +30,7 @@ while True:
 		f.write('\nsolution:\n')
 		f.write(solution)
 	
+	# submit the solution to CodinGame
 	r = session.post('https://www.codingame.com/services/TestSession/play', json=[handle, {'code':solution, 'programmingLanguageId':'PHP', 'multipleLanguages':{'testIndex':1}}])
 	print('replay: https://www.codingame.com/replay/' + str(r.json()['gameId']))
 	next_level = ''
@@ -41,6 +45,8 @@ while True:
 		f.write(next_level.split('\n')[0])
 	with open('level.txt', 'w') as f:
 		f.write('\n'.join(next_level.split('\n')[1:]))
+	
+	# save input for next level
 	with open('log.txt', 'a') as f:
 		f.write('\nreplay: https://www.codingame.com/replay/' + str(r.json()['gameId']))
 		f.write('\n\nLevel ' + str(int(1 + r.json()['metadata']['Level'])) + ':\n')
